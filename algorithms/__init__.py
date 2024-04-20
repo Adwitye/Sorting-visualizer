5cf61e002d6052da004d34379acde684e4a9fd3b
Aditya Jha <asjha2003@gmail.com>
2024-04-20 14:43:35 +0530

    first commit

---
+++ algorithms/__init__.py
@@ -0,0 +1,5 @@
+from algorithms.bubbleSort import bubble_sort
+from algorithms.mergeSort import merge_sort
+from algorithms.insertionSort import insertion_sort
+from algorithms.selectionSort import selection_sort
+from algorithms.quickSort import quick_sort
